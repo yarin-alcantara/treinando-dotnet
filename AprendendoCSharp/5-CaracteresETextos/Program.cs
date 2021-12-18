ad1d20b5383f7f08686b2b32ba43f1eba58b1497
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2021-12-18 16:21:05 -0300

    Caracteres, texto e variaveis

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+char primeiraLetra = 'a';
+Console.WriteLine(primeiraLetra);
+
+
+primeiraLetra = (char)65;
+Console.WriteLine(primeiraLetra);
+
+primeiraLetra = (char)(primeiraLetra + 1);
+Console.WriteLine(primeiraLetra);
+
+string titulo = "Alura Cursos de tecnologia " + 2021;
+string cursos = " - .NET \n" +
+    " - Java \n" +
+    " - Javascript \n" +
+    " - C++ ";
+Console.WriteLine(titulo);
+Console.WriteLine(cursos);
+
+
+
+Console.ReadLine();
