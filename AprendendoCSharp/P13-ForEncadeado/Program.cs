e893dc94b67ce7e4805dd114905b8bf17d77f94f
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2021-12-19 17:35:42 -0300

    Finalizando primeiros passos C#

--- AprendendoCSharp/P13-ForEncadeado/Program.cs
+++ AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -20,7 +20,6 @@ for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
     for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
     {
         Console.Write("*");
-   
     }
     Console.WriteLine();
 }
