3eb534701f030bd5f23abaae3ee5df7dfe1b58ba
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2021-12-19 17:47:13 -0300

    Desafios c# com FOR

---
+++ AprendendoCSharp/Fatorial/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Desafio de Fatorial de 1 a 10");
+
+int fatorial = 1;
+
+for (int i = 1; i < 11; i++)
+{
+    fatorial *= i;
+    Console.WriteLine("O fatorial de "+ i + " é igual a: "+ fatorial);
+}
---
+++ AprendendoCSharp/Multiplos3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Desafio de Multiplos de 3");
+
+for(int i = 1; i < 100; i++)
+{
+    if (i %3 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
