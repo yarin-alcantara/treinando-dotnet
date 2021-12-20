825921b352c121aeeca1f1b2669310583d8e3598
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2021-12-20 19:09:36 -0300

    Criando uma classe

--- Bitebank/01-Bitebank/Program.cs
+++ Bitebank/01-Bitebank/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ContaCorrente contaDaGabriela = new ContaCorrente();
+
+contaDaGabriela.titular = "Gabriela";
+contaDaGabriela.agencia = 555;
+contaDaGabriela.numero = 9876564;
+contaDaGabriela.saldo = 1000;
+
+
+Console.WriteLine("Titular: "+contaDaGabriela.titular);
+Console.WriteLine("Agência: "+contaDaGabriela.agencia);
+Console.WriteLine("Número: "+contaDaGabriela.numero);
+Console.WriteLine("Saldo: "+contaDaGabriela.saldo);
+
+contaDaGabriela.saldo += 1000;
+Console.WriteLine("Novo saldo: "+contaDaGabriela.saldo);
+
+Console.ReadLine();
+
+
+
